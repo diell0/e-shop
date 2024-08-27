b383c71c5d11fe6d7e64842d7a29af6089378fae
diell0 <sh48714@ubt-uni.net>
2024-08-27 19:19:25 +0200

    added favorites component

---
+++ src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,13 @@
+import "./Favorites.scss";
+import React from "react";
+import { Flex } from "antd";
+
+const Favorites = () => {
+  return (
+    <Flex className="favoritesContainer">
+      <h1>Favorites</h1>
+    </Flex>
+  );
+};
+
+export default Favorites;
